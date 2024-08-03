ca3b7a83ff6c1da288073970439a5a9695b7753d
Ayvan <143087848+Ayvan-Bem@users.noreply.github.com>
2024-08-03 09:37:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+'use strict';
+const themeToggleBtn = document.querySelector('#theme-toggle-btn');
+const themeIcon = document.querySelector('#theme-icon');
+
+themeToggleBtn.addEventListener('click', function () {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if (className === "light-theme") {
+        themeIcon.innerHTML = `
+            <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+        `;
+    } else {
+        themeIcon.innerHTML = `
+            <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+        `;
+    }
+    console.log('current class name:' + className);
+});
+
+window.addEventListener('scroll', function () {
+    const splineViewer = document.getElementById('spline-viewer');
+    const scrollPosition = window.scrollY;
+    const offset = scrollPosition * 0.5; // Adjust the multiplier for desired parallax effect
+
+    splineViewer.style.transform = `translateY(${offset}px)`;
+});
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function () {
+    itemActive = itemActive + 1;
+    if (itemActive >= countItem) {
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function () {
+    itemActive = itemActive - 1;
+    if (itemActive < 0) {
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider() {
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
+document.getElementById('redirectButton').addEventListener('click', function () {
+    window.location.href = 'https://youtu.be/cS5xX7tHtgM'; // Replace with your desired URL
+});
+document.getElementById('redirectButton2').addEventListener('click', function () {
+    window.location.href = 'https://youtu.be/fFSY_3y3L6A'; // Replace with your desired URL
+});
